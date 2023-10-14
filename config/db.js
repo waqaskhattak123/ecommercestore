3ebb117b9d18e8bcd2ef31c4f6fd448e1bcb8065
SamiKhan35 <Sami_Khan@gmail.com>
2023-10-14 16:07:35 +0500

    Signup with Hashed Password

--- config/db.js
+++ config/db.js
@@ -1,1 +1,13 @@
 const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+        });
+        console.log(`Connected With MongoDB ${conn.connection.host}`.cyan.underline.bold);
+    } catch (error) {
+        console.log(`Error in Connecting with MongoDB ${error}`);
+    }
+}
+module.exports = connectDB;
